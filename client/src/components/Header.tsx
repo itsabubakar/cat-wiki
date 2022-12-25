09e8b46eaafefe35025bd49ddb046eb2fa28d9a4
Sadiq <seeker99157@gmail.com>
2022-12-25 14:53:50 +0100

    roll up doesnt like svg in image

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
-import catWikiLogo from '../assets/CatWikiLogo.svg'
+// import catWikiLogo from '../assets/CatWikiLogo.svg'
 
 const Header = () => {
     return (
         <header className='mb-4'>
-            <Link to='/'><img src={catWikiLogo} alt="" /></Link>
+            {/* <Link to='/'><img src={catWikiLogo} alt="" /></Link> */}
+            <Link to='/'>Home page</Link>
         </header>
     )
 }
